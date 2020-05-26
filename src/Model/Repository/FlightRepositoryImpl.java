4dbecc91521c087cad0a3c8a121c082711546ac3
Eyal17 <eyalgeron94@gmail.com>
2020-05-26 14:00:29 +0300

    26/5 update table

--- src/Model/Repository/FlightRepositoryImpl.java
+++ src/Model/Repository/FlightRepositoryImpl.java
@@ -9,11 +9,15 @@ public class FlightRepositoryImpl implements templateRepository<Integer, Flight>
 	Map<Integer, Flight> flights = new HashMap<Integer, Flight>();
 	@Override
 	public void add(Flight v) {
-		flights.put(v.getFlightID(), v);
+		//flights.put(v.getFlightID(), v);
+		String query = "INSERT INTO flightboard(flightid,planeid)" + "VALUES (" + v.getFlightID() + "," + v.getPlane().getPlaneID() + ")";
+		DBManager.addToDB(query);
 	}
 	@Override
-	public void delete(Integer k) {
-		flights.remove(k);		
+	public void delete(Integer id) {
+		//flights.remove(k);		
+		String query = "DELETE from flightboard WHERE flightboard.flightid=" + id;
+		DBManager.deleteFromDB(query);
 	}
 	@Override
 	public void print() {
@@ -29,4 +33,6 @@ public class FlightRepositoryImpl implements templateRepository<Integer, Flight>
 	{
 		
 	}
+	
+	
 }
