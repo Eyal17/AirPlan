a919ba8b3b8b39be88897583ce5c10e55e6dbbb1
Amir Goldstein <amirx111994@gmail.com>
2020-06-01 13:09:50 +0300

    Model class

---
+++ src/Model/Repository/ModelRepository.java
@@ -0,0 +1,9 @@
+package Model.Repository;
+
+public class ModelRepository {
+
+	public AirportRepositoryImpl airportRepo;
+	public FleetRepositoryImpl fleetRepo;
+	public FlightRepositoryImpl flightRepo;
+
+}
