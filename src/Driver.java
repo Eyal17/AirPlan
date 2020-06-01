1b1cf119a2907546435395af5557fd5a8e296507
omerborshan <omerborshan@gmail.com>
2020-06-01 13:16:27 +0300

    driver class creation

---
+++ src/Driver.java
@@ -0,0 +1,20 @@
+import java.awt.EventQueue;
+
+import View.HomeBuilder;
+
+public class Driver {
+	ModelRepository model;
+	View view;
+	Controller controller;
+	public static void main(String[] args)
+	{
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+	}
+}
